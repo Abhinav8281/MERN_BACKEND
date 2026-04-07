b867cc4c71b52b7da7c37d85b14a62757a8932e4
abhinav <abhinavkrishnakp005@gmail.com>
2026-04-07 11:23:17 +0530

    day_5

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const errorMiddleware = require("./middleware/errorMiddleware");
 const AppError = require("./utils/AppError");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+
 
 dotenv.config();
 connectDB();
@@ -19,7 +23,18 @@ app.use( (req, res, next) => {
   next(new AppError(`Route ${req.originalUrl} not found`, 404));
 });
 
+// create write stream for logs
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "combined.log"),
+  { flags: "a" }
+);
+
+// use morgan
+app.use(morgan("combined", { stream: accessLogStream }));
+
 // ✅ Error Middleware (MUST BE LAST)
 app.use(errorMiddleware);
+
+
 
 app.listen(5000, () => console.log("Server running"));
